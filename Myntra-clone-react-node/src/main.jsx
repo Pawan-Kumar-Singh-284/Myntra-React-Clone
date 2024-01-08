b3f072afce1cdf9d5b22870c19d3deacde1017de
Pawan-Kumar-Singh-284 <19krpawan@gmail.com>
2024-01-08 22:29:21 +0530

    first commit

---
+++ Myntra-clone-react-node/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import Bag from "./routes/Bag.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/bag", element: <Bag /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
